49da821ee5e7d18921d91c2baa092f0254a56890
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-04-03 14:45:04 +0530

    Update ProjectMemberMng.jsx

--- FrontEnd/src/pages/co-ordinator/ProjectMemberMng/ProjectMemberMng.jsx
+++ FrontEnd/src/pages/co-ordinator/ProjectMemberMng/ProjectMemberMng.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import CoordinatorWelcomeCard from '../../../components/CoordinatorWelcomeCard';
 import { useNavigate } from 'react-router-dom';
